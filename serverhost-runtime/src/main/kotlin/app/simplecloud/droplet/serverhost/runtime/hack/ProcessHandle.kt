869185e5c72ab31f882ca583485a6a51449d0b3d
Philipp <p.eistrach@gmail.com>
2024-04-02 18:12:14 +0200

    refactor: make process handle to object

--- serverhost-runtime/src/main/kotlin/app/simplecloud/droplet/serverhost/runtime/hack/ProcessHandle.kt
+++ serverhost-runtime/src/main/kotlin/app/simplecloud/droplet/serverhost/runtime/hack/ProcessHandle.kt
@@ -7,59 +7,58 @@ import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import java.util.regex.Pattern
 
-class PortProcessHandle {
-    companion object {
+object PortProcessHandle {
 
-        private val preBindPorts = ConcurrentHashMap<Int, LocalDateTime>()
+    private val preBindPorts = ConcurrentHashMap<Int, LocalDateTime>()
 
-        fun of(port: Int): Optional<ProcessHandle> {
-            val os = OS.get() ?: return Optional.empty()
-            val command: String
-            val pattern: Pattern
-            val pidIndex: Int
-            when (os) {
-                OS.WINDOWS -> {
-                    command = "cmd /c netstat -a -n -o | findstr $port"
-                    pattern = Pattern.compile("\\s*TCP\\s+\\S+:(\\d+)\\s+\\S+:(\\d+)\\s+\\S+\\s+(\\d+)")
-                    pidIndex = 3
-                }
-
-                OS.UNIX -> {
-                    command = "bash -c lsof -i :$port"
-                    pattern = Pattern.compile("\\S+\\s+(\\d+)\\s+.*:$port")
-                    pidIndex = 1
-                }
+    fun of(port: Int): Optional<ProcessHandle> {
+        val os = OS.get() ?: return Optional.empty()
+        val command: String
+        val pattern: Pattern
+        val pidIndex: Int
+        when (os) {
+            OS.WINDOWS -> {
+                command = "cmd /c netstat -a -n -o | findstr $port"
+                pattern = Pattern.compile("\\s*TCP\\s+\\S+:(\\d+)\\s+\\S+:(\\d+)\\s+\\S+\\s+(\\d+)")
+                pidIndex = 3
             }
 
-            val process = Runtime.getRuntime().exec(command)
-            val reader = process.inputReader()
-            var line = ""
-            while (reader.readLine()?.also { line = it } != null) {
-                val matcher = pattern.matcher(line)
-                if (matcher.matches()) {
-                    preBindPorts.remove(port)
-                    val pid: Long = matcher.group(pidIndex).toLong()
-                    return ProcessHandle.of(pid)
-                }
+            OS.UNIX -> {
+                command = "bash -c lsof -i :$port"
+                pattern = Pattern.compile("\\S+\\s+(\\d+)\\s+.*:$port")
+                pidIndex = 1
             }
-            return Optional.empty()
         }
 
-        @Synchronized
-        fun findNextFreePort(startPort: Int, serverDefinition: ServerDefinition): Int {
-            val server = Server.fromDefinition(serverDefinition)
-            var port = startPort
-            while (!of(port).isEmpty || LocalDateTime.now()
-                    .isBefore(preBindPorts.getOrDefault(port, LocalDateTime.MIN))
-            ) {
-                port++
+        val process = Runtime.getRuntime().exec(command)
+        val reader = process.inputReader()
+        var line = ""
+        while (reader.readLine()?.also { line = it } != null) {
+            val matcher = pattern.matcher(line)
+            if (matcher.matches()) {
+                preBindPorts.remove(port)
+                val pid: Long = matcher.group(pidIndex).toLong()
+                return ProcessHandle.of(pid)
             }
-            addPreBind(port, server.createdAt, server.properties.getOrDefault("max-startup-seconds", "20").toLong())
-            return port
         }
-
-        private fun addPreBind(port: Int, time: LocalDateTime, duration: Long) {
-            preBindPorts[port] = time.plusSeconds(duration)
-        }
+        return Optional.empty()
     }
+
+    @Synchronized
+    fun findNextFreePort(startPort: Int, serverDefinition: ServerDefinition): Int {
+        val server = Server.fromDefinition(serverDefinition)
+        var port = startPort
+        while (!of(port).isEmpty || LocalDateTime.now()
+                .isBefore(preBindPorts.getOrDefault(port, LocalDateTime.MIN))
+        ) {
+            port++
+        }
+        addPreBind(port, server.createdAt, server.properties.getOrDefault("max-startup-seconds", "20").toLong())
+        return port
+    }
+
+    private fun addPreBind(port: Int, time: LocalDateTime, duration: Long) {
+        preBindPorts[port] = time.plusSeconds(duration)
+    }
+
 }
