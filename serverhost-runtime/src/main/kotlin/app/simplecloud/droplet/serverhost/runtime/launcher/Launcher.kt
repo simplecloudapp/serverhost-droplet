13ef94d26ecbf6dc2030c680bcd10dcac495df9b
Frederick Baier <baier.frederick@gmail.com>
2024-04-22 21:46:07 +0200

    feat: add log4j error handling

--- serverhost-runtime/src/main/kotlin/app/simplecloud/droplet/serverhost/runtime/launcher/Launcher.kt
+++ serverhost-runtime/src/main/kotlin/app/simplecloud/droplet/serverhost/runtime/launcher/Launcher.kt
@@ -1,5 +1,15 @@
 package app.simplecloud.droplet.serverhost.runtime.launcher
 
+import org.apache.logging.log4j.LogManager
+
 fun main(args: Array<String>) {
+    configureLog4j()
     ServerHostStartCommand().main(args)
+}
+
+fun configureLog4j() {
+    val globalExceptionHandlerLogger = LogManager.getLogger("GlobalExceptionHandler")
+    Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+        globalExceptionHandlerLogger.error("Uncaught exception in thread ${thread.name}", throwable)
+    }
 }
