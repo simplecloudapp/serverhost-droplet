7fed08338463052179df347079c2c8f9d818c1d6
Dayeeet <david@cappell.net>
2024-04-26 15:11:28 +0200

    add: warning if template is recursive

--- serverhost-runtime/src/main/kotlin/app/simplecloud/droplet/serverhost/runtime/template/TemplateCopier.kt
+++ serverhost-runtime/src/main/kotlin/app/simplecloud/droplet/serverhost/runtime/template/TemplateCopier.kt
@@ -3,6 +3,7 @@ package app.simplecloud.droplet.serverhost.runtime.template
 import app.simplecloud.controller.shared.server.Server
 import app.simplecloud.droplet.serverhost.runtime.launcher.ServerHostStartCommand
 import app.simplecloud.droplet.serverhost.runtime.runner.ServerRunner
+import org.apache.logging.log4j.LogManager
 import java.nio.file.Path
 
 class TemplateCopier(
@@ -10,6 +11,7 @@ class TemplateCopier(
 ) {
 
     private val loadedTemplates = mutableMapOf<String, Template>()
+    private val logger = LogManager.getLogger(TemplateCopier::class)
 
     fun copy(server: Server, runner: ServerRunner, actionType: TemplateActionType) {
         var templateId = server.properties["template-id"]
@@ -36,7 +38,10 @@ class TemplateCopier(
     }
 
     private fun loadTemplate(id: String, loaded: MutableList<String> = mutableListOf(), current: Template? = null): Template? {
-        if(loaded.contains(id)) return current
+        if(loaded.contains(id)) {
+            logger.warn("Template $id could not be loaded correctly. Did you make your template recursive by accident?")
+            return current
+        }
         val template = loadedTemplates.getOrDefault(id, null) ?: return current
         val existing = current ?: template
         if(existing != template) {
