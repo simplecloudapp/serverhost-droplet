f8b5f55fe1ff209859979db915206bba6e72099e
Dayeeet <david@cappell.net>
2024-04-28 13:31:30 +0200

    fix: file system issues

--- serverhost-runtime/src/main/kotlin/app/simplecloud/droplet/serverhost/runtime/resources/ResourceCopier.kt
+++ serverhost-runtime/src/main/kotlin/app/simplecloud/droplet/serverhost/runtime/resources/ResourceCopier.kt
@@ -1,26 +1,37 @@
 package app.simplecloud.droplet.serverhost.runtime.resources
 
-import java.nio.file.Files
-import java.nio.file.StandardCopyOption
+import java.io.File
+import java.net.URI
+import java.nio.file.*
 import kotlin.io.path.*
+
 
 class ResourceCopier {
 
     @OptIn(ExperimentalPathApi::class)
     fun copyAll(dir: String) {
         val url = ResourceCopier::class.java.getResource("/$dir") ?: return
-
-        val path = url.toURI().toPath()
+        val uri = url.toURI()
+        val fileSystem = if(uri.scheme == "jar") createFileSystem(uri) else null
+        val path = Paths.get(uri)
         if(!path.exists()) return
         if(!path.isDirectory()) return
         path.listDirectoryEntries().forEach {
-            it.walk(PathWalkOption.BREADTH_FIRST).forEach { child ->
-                val relative = child.relativeTo(path)
+            it.walk(PathWalkOption.FOLLOW_LINKS).forEach { child ->
+                val relative = File(child.relativeTo(path).pathString).toPath()
                 if(!relative.parent.exists())
                     Files.createDirectories(relative)
                 Files.copy(child, relative, StandardCopyOption.REPLACE_EXISTING)
             }
         }
+        fileSystem?.close()
+    }
+
+    private fun createFileSystem(uri: URI): FileSystem {
+        val env: MutableMap<String, String> = HashMap()
+        env["create"] = "true"
+        env["encoding"] = "UTF-8"
+        return FileSystems.newFileSystem(uri, env)
     }
 
 }
