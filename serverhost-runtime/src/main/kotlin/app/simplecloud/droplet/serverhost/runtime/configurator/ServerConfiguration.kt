e911284d1d8e096e6f2a0165b696e39dcb5b7718
Philipp <p.eistrach@gmail.com>
2024-05-24 17:50:28 +0200

    refactor: add group and numerical id to configurator placeholders

--- serverhost-runtime/src/main/kotlin/app/simplecloud/droplet/serverhost/runtime/configurator/ServerConfiguration.kt
+++ serverhost-runtime/src/main/kotlin/app/simplecloud/droplet/serverhost/runtime/configurator/ServerConfiguration.kt
@@ -39,6 +39,8 @@ object ServerConfiguratorPlaceholderMapper {
         val map = mutableMapOf<String, Any>()
         map["ip"] = server.ip
         map["port"] = server.port
+        map["group"] = server.group
+        map["numerical-id"] = server.numericalId
         map["max-players"] = server.maxPlayers
         map.putAll(server.properties)
         return map
