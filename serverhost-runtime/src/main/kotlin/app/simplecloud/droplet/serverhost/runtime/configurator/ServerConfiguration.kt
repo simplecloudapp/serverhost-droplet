627a514a77304870ac19fcda2fc23c6ad8016264
Dayeeet <david@cappell.net>
2024-05-26 13:44:30 +0200

    refactor: remove unused imports

--- serverhost-runtime/src/main/kotlin/app/simplecloud/droplet/serverhost/runtime/configurator/ServerConfiguration.kt
+++ serverhost-runtime/src/main/kotlin/app/simplecloud/droplet/serverhost/runtime/configurator/ServerConfiguration.kt
@@ -1,11 +1,7 @@
 package app.simplecloud.droplet.serverhost.runtime.configurator
 
-import app.simplecloud.controller.shared.server.Server
-import app.simplecloud.droplet.serverhost.runtime.config.YamlConfig
-import app.simplecloud.droplet.serverhost.runtime.runner.ServerRunner
 import org.spongepowered.configurate.objectmapping.ConfigSerializable
 import org.spongepowered.configurate.objectmapping.meta.Setting
-import java.io.File
 
 
 @ConfigSerializable
