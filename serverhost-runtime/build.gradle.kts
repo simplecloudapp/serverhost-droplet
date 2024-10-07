f3c6c46f0aa24627c23b383df5cd524fd28199d5
David <david@cappell.net>
2024-10-07 18:21:42 +0200

    refactor: make serverhost runtime more robust and change env name for pubsub connections

--- serverhost-runtime/build.gradle.kts
+++ serverhost-runtime/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
     application
 }
@@ -12,19 +10,6 @@ dependencies {
     implementation(rootProject.libs.clikt)
     implementation(rootProject.libs.commonsIo)
     implementation(rootProject.libs.toml4j)
-}
-
-tasks.named("shadowJar", ShadowJar::class) {
-    dependencies {
-        include(dependency(rootProject.libs.kotlinCoroutines.get()))
-        include(dependency(rootProject.libs.log4jApi.get()))
-        include(dependency(rootProject.libs.log4jCore.get()))
-        include(dependency(rootProject.libs.log4jSlf4j.get()))
-        include(dependency(rootProject.libs.clikt.get()))
-        include(dependency(rootProject.libs.commonsIo.get()))
-        include(dependency(rootProject.libs.toml4j.get()))
-    }
-    archiveFileName.set("${project.name}.jar")
 }
 
 application {
