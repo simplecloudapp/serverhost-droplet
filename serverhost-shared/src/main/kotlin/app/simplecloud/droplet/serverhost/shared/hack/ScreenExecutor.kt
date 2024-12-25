2ec1e151b45213fac34b79e1602bacf74b64ea0f
dayeeet <david@cappell.net>
2024-12-26 00:49:39 +0100

    refactor: update readme

--- serverhost-shared/src/main/kotlin/app/simplecloud/droplet/serverhost/shared/hack/ScreenExecutor.kt
+++ serverhost-shared/src/main/kotlin/app/simplecloud/droplet/serverhost/shared/hack/ScreenExecutor.kt
@@ -6,7 +6,9 @@ class ScreenExecutor(private var pid: Long) {
     init {
         var handle = ProcessHandle.of(pid)
         while (handle.isPresent) {
-            if (handle.get().info().commandLine().orElseGet { "" }.lowercase().startsWith("screen")) {
+            if (handle.get().info().command().orElseGet { "" }.lowercase().startsWith("screen") || handle.get().info()
+                    .arguments().orElseGet { arrayOf("none") }[0].lowercase().startsWith("screen")
+            ) {
                 pid = handle.get().pid()
                 isScreen = true
                 break
@@ -20,7 +22,7 @@ class ScreenExecutor(private var pid: Long) {
     }
 
     fun sendCommand(toSend: Array<String>) {
-        if(!isScreen) return
+        if (!isScreen) return
         val command = arrayOf("screen", "-S", pid.toString(), "-X", *toSend)
         Runtime.getRuntime().exec(command)
     }
